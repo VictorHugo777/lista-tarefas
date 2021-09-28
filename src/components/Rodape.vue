3ff87a5690036d5d4dbc913a528f7570fcdf348b
VictorHugo777 <agostinivictor60@gmail.com>
2021-09-27 22:45:09 -0300

    lista-tarefas-atualizada

---
+++ src/components/Rodape.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+      Meu Rodapé
+  </footer>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+footer{
+    background-color: royalblue;
+    height: 50px;
+    width: 100%;
+    position: fixed;
+    bottom: 0px;
+}
+
+</style>
